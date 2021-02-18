d19569669f3f958ce390a35df9c45f9c3cbba67c
panshuljindal <60618820+panshuljindal@users.noreply.github.com>
2021-02-18 16:03:27 +0530

    added navbar1

---
+++ Hackgrid/app/src/main/java/com/adgvit/hackgrid/MainActivity.java
@@ -0,0 +1,49 @@
+package com.adgvit.hackgrid;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+    BottomNavigationView bottomNavigationView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.nav_view);
+        getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout,new timeline()).commit();
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment selectedfragment = null;
+                switch (item.getItemId()){
+                    case R.id.navigation_timeline:
+                        selectedfragment = new timeline();
+                        break;
+                    case R.id.navigation_partners:
+                        selectedfragment = new partners();
+                        break;
+                    case R.id.navigation_tracks:
+                        selectedfragment = new tracks();
+                        break;
+                    case R.id.navigation_faq:
+                        selectedfragment = new faq();
+                        break;
+                    case R.id.navigation_profile:
+                        selectedfragment = new profile();
+
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout,selectedfragment).commit();
+                return  true;
+            }
+        });
+
+
+    }
+}
